c178fb8dbcf7712d905f2aeef5e32fd00326f7a8
jinuk <wingband4980@gmail.com>
2018-10-30 19:12:18 +0100

    출력문 추가

--- Jinuk/src/HelloJava.java
+++ Jinuk/src/HelloJava.java
@@ -9,7 +9,7 @@ public class HelloJava {
 		System.out.println("Time is 20:00");
 		System.out.println("so coolll");
 		System.out.println("아름다운 밤이야");
-		
+		System.out.println("아름다운 밤이야11");
 	}
 
 }
